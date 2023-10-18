f5755bee74b2995615e879a0b83220d9d5084754
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-18 10:05:39 +0200

    Uppg a

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int litetTal=10;
+
+        if(litetTal<1000)
+        {
+            int stortTal=1000;
+            System.out.println(litetTal+" är större än "+stortTal);
+        }
+        else
+        {
+            System.out.println(litetTal+" är mindre än "+stortTal);
+        }
+        System.out.println("litetTal="+litetTal+", stortTal="+stortTal);
+
+        //StortTal är inte definierad i alla kroppar.
+}
+
