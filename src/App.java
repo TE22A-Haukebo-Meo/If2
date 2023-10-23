1f854e63e83979422c28c57290c3e3b3e57e6a55
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-23 08:41:17 +0200

    Uppgift f klar

--- src/App.java
+++ src/App.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ålder?");
+        //System.out.println("Ålder?");
         Scanner tb = new Scanner(System.in);
-        int age = tb.nextInt();
-        System.out.println("Äru medlem");
-            tb.nextLine();
-        String medlem = tb.nextLine();
+        //int age = tb.nextInt();
+//        System.out.println("Äru medlem");
+//          tb.nextLine();
+//      String medlem = tb.nextLine();
+//
+//        if(age>=18)
+//        {
+//            System.out.println("Ajde kom in");
+//        }
+//        else if(medlem.equalsIgnoreCase("ja"))
+//        {
+//            System.out.println("Ajde kom in");
+//        }
+//        else{
+//            System.out.println("Jalla tagga härifrån");
+//        }
+        System.out.println("Ange Användarnamn");
+        String user = tb.nextLine();
+        System.out.println("Ange lösenord");
+        String pass = tb.nextLine();
 
-        if(age>=18)
+        if(user.equals("root")  && (pass.equals("passwd")))
         {
-            System.out.println("Ajde kom in");
-        }
-        else if(medlem.equalsIgnoreCase("ja"))
-        {
-            System.out.println("Ajde kom in");
-        }
+            System.out.println("VÄlkommen tillbaka root");
+        } 
         else{
-            System.out.println("Jalla tagga härifrån");
+            System.out.println("Fel användarnamn eller lösenord.");
         }
     }
 }
